4b08159c036a26f83015d3d58520bb7619abf7fb
I3-Parth <parth.shah@techprescient.com>
2024-03-05 14:47:34 +0530

    f1: Mapper added

---
+++ src/main/java/com/example/EnumDemo/mapper/DressesMapper.java
@@ -0,0 +1,34 @@
+package com.example.EnumDemo.mapper;
+
+import com.example.EnumDemo.dto.DressAdditionDto;
+import com.example.EnumDemo.dto.DressDisplayDto;
+import com.example.EnumDemo.enumPackage.Size;
+import com.example.EnumDemo.model.DressEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface DressesMapper {
+
+    @Mapping(source = "brand", target = "brand")
+    @Mapping(source = "type", target = "type")
+    @Mapping(target = "size", expression = "java(convertSizeIntToEnum(dressAdditionDto))")
+    @Mapping(source = "color", target = "color")
+    @Mapping(source = "price", target = "price")
+    DressEntity convertDressAdditionDtoToDressEntity(DressAdditionDto dressAdditionDto);
+
+    default Size convertSizeIntToEnum(DressAdditionDto dressAdditionDto){
+        int currentSize = dressAdditionDto.getSize();
+        if(currentSize<=30) return Size.S;
+        else if (currentSize>30 && currentSize<50) return Size.M;
+        else return Size.L;
+    }
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "brand", target = "brand")
+    @Mapping(source = "type", target = "type")
+    @Mapping( source = "size", target = "size")
+    @Mapping(source = "color", target = "color")
+    @Mapping(source = "price", target = "price")
+    DressDisplayDto convertDressEntityToDressDisplayDto(DressEntity dressEntity);
+}
